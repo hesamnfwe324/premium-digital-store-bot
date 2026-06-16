2dfaed5544e7be75fdb49d1f145f087333bd133f
hesamnfwe324 <hasm95037@gmail.com>
2026-06-17 01:03:37 +0330

    Add database/models/__init__.py

---
+++ database/models/__init__.py
@@ -0,0 +1,25 @@
+from .user import User
+from .product import Product, ProductCategory, ProductType
+from .order import Order, OrderStatus
+from .payment import Payment, PaymentStatus, CryptoCurrency
+from .wallet import Wallet
+from .transaction import Transaction, TransactionType
+from .gift_card import GiftCard
+from .visa_card import VisaCard
+from .master_card import MasterCard
+from .ticket import Ticket, TicketStatus
+from .referral import Referral
+from .discount_code import DiscountCode, DiscountType
+from .setting import Setting
+from .language import Language
+
+__all__ = [
+    "User", "Product", "ProductCategory", "ProductType",
+    "Order", "OrderStatus",
+    "Payment", "PaymentStatus", "CryptoCurrency",
+    "Wallet", "Transaction", "TransactionType",
+    "GiftCard", "VisaCard", "MasterCard",
+    "Ticket", "TicketStatus",
+    "Referral", "DiscountCode", "DiscountType",
+    "Setting", "Language"
+]
